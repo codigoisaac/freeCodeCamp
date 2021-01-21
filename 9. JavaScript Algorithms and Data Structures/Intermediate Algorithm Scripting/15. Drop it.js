ef4a444a9dec48dcbf341d8a8d2c9e29cac22b0a
Isaac Muniz <codigoisaac@yahoo.com>
2021-01-21 14:21:29 -0300

    Create 15. Drop it.js

---
+++ 9. JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/15. Drop it.js
@@ -0,0 +1,12 @@
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+function dropElements(arr, func) {
+  let indexPassed = arr.findIndex((elem) => func(elem));
+  return indexPassed >= 0 ? arr.slice(indexPassed) : [];
+}
+
+dropElements([1, 2, 3], function (n) {
+  return n < 3;
+});
